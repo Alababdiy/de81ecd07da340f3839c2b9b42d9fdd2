90907f792016b3c01f6668b1d9bd0c6cf375c0e4
abdallah <okupu21@okupu.nz>
2021-09-30 14:05:01 +0300

    normal

--- assets/frontend/controller.js
+++ assets/frontend/controller.js
@@ -1,1 +1,20 @@
-const APP_DOMAIN = 'https://app.jeet.sa';
+if (['/','','index.html','index'].includes(page)){
+    function subscription(){
+        $.post(
+            '/email_subscription',
+            $('form').serialize(),
+            function (data) {
+                console.log(data);
+                data = response_handler(data);
+                if(data.success){
+                    $('#sub_form_message').html('تم الاشتراك ، شكراً ✅');
+                    $('form').remove();
+                }
+                else{
+                    $('#sub_form_message').html("فشلت العملية");
+                }
+            }
+        )
+    }
+}
+
---
+++ assets/frontend/func.js
@@ -0,0 +1,88 @@
+
+function isLocalNetwork(hostname = window.location.hostname) {
+    return (
+        (['localhost', '127.0.0.1', '', '::1'].includes(hostname))
+        || (hostname.startsWith('192.168.'))
+        || (hostname.startsWith('10.0.'))
+        || (hostname.endsWith('.local'))
+    )
+}
+const segments = new URL(window.location.href).pathname.split('/');
+const page = segments.pop() || segments.pop();
+
+
+
+let APP_DOMAIN
+if (isLocalNetwork())
+    APP_DOMAIN = 'http://127.0.0.1:5000';
+else
+    APP_DOMAIN = 'https://app.jeet.sa';
+
+
+function url(path){
+    return APP_DOMAIN+path
+}
+
+/**
+ * @return {HTMLElement}
+ */
+function insertParam(key, value) {
+    key = encodeURIComponent(key);
+    value = encodeURIComponent(value);
+
+    // kvp looks like ['key1=value1', 'key2=value2', ...]
+    var kvp = document.location.search.substr(1).split('&');
+    let i = 0;
+
+    for (; i < kvp.length; i++) {
+        if (kvp[i].startsWith(key + '=')) {
+            let pair = kvp[i].split('=');
+            pair[1] = value;
+            kvp[i] = pair.join('=');
+            break;
+        }
+    }
+
+    if (i >= kvp.length) {
+        kvp[kvp.length] = [key, value].join('=');
+    }
+
+    // can return this or...
+    let params = kvp.join('&');
+
+    // reload page with new params
+    document.location.search = params;
+}
+
+function el(id) {
+    return document.getElementById(id)
+}
+
+function value(id) {
+    return document.getElementById(id).value
+}
+
+function content(id) {
+    return document.getElementById(id)
+}
+
+function response_handler(data) {
+
+    let res = {}
+    if (data === 'ok')
+        res.success = true
+    else {
+        data = JSON.parse(data);
+        if (data.status)
+            data.success = true
+        res = data
+        if ('action' in data)
+            eval(data['action'])
+        if ('message' in data)
+            message(data['message']);
+        if ('redirect' in data)
+            redirect(data['redirect'])
+    }
+    
+    return res
+}
